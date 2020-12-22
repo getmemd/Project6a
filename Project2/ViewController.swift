b875719f0b5c8adb39ceb53012b83b369f9917fb
Adilkhan Medeuyev <a.medeuyev@aviata.me>
2020-12-23 01:29:19 +0600

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Адиль Медеуев on 23.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
